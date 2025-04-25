8aefc5af46f640f3fcd7b33a877570f602535e6a
luis-gouveia <luis.gouveia2801@gmail.com>
2025-04-25 13:47:27 +0100

    chore: weather day modal improvements

--- src/components/WeatherDayModal/WeatherDayModal.tsx
+++ src/components/WeatherDayModal/WeatherDayModal.tsx
@@ -1,21 +1,32 @@
-import { Button, Col, Container, Modal, Row } from 'react-bootstrap'
+import { JSX } from 'react'
+import { Button, Col, Container, Modal, OverlayTrigger, Row, Tooltip } from 'react-bootstrap'
 import WeatherCard from '../WeatherCard/WeatherCard'
-import DotIcon from '../../assets/uiIcons/dot.svg?react'
-import LeftArrowIcon from '../../assets/uiIcons/left_arrow.svg?react'
-import WarningIcon from '../../assets/uiIcons/warning.svg?react'
-import './WeatherDayModal.css'
+import DotIcon from '../../assets/ui/dot.svg?react'
+import LeftArrowIcon from '../../assets/ui/left_arrow.svg?react'
+import WarningIcon from '../../assets/ui/warning.svg?react'
 import { DateUtils } from '../../utils/DateUtils'
 import { Warning } from '../../types/Warning'
 import { WeatherData } from '../../types/WeatherData'
+import './WeatherDayModal.css'
 
 interface WeatherDayModalProps {
-  show: boolean
-  onClose: () => void
   location: string
-  weatherData: WeatherData
+  weatherData?: WeatherData
+  onClose: () => void
 }
 
-const WarningLine = ({ warning }: { warning: Warning }) => {
+/**
+ * Renders a single warning line item showing the warning's type, name, description, and time range.
+ *
+ * @component
+ * @param {Object} props Component props
+ * @param {Warning} props.warning The warning object
+ *
+ * @returns {JSX.Element} The rendered warning line.
+ */
+const WarningLine = ({ warning }: { warning: Warning }): JSX.Element => {
+  const descriptionTooltip = <Tooltip className="description-tooltip">{warning.description}</Tooltip>
+
   return (
     <Container className="warning-line">
       <Row>
@@ -25,7 +36,15 @@ const WarningLine = ({ warning }: { warning: Warning }) => {
         <Col xs={7}>
           <Container className="p-0">
             <Row className="warning-title">{warning.name}</Row>
-            <Row className="warning-description">{warning.description}</Row>
+            {warning.description.length >= 32 ? (
+              <OverlayTrigger overlay={descriptionTooltip} placement="top">
+                <Row className="warning-description">{`${warning.description.slice(0, 33)}...`}</Row>
+              </OverlayTrigger>
+            ) : (
+              <Row className="warning-description">
+                {warning.description.length ? warning.description : 'Sem detalhes'}
+              </Row>
+            )}
           </Container>
         </Col>
         <Col xs={1}>
@@ -39,9 +58,21 @@ const WarningLine = ({ warning }: { warning: Warning }) => {
   )
 }
 
-const WeatherDayModal = ({ show, onClose, location, weatherData }: WeatherDayModalProps) => {
+/**
+ * Modal component that displays weather information for a specific day and location.
+ * Includes an expanded weather card and a list of weather warnings if present.
+ *
+ * @component
+ * @param {Object} props Component props
+ * @param {string} props.location The name of the selected location.
+ * @param {WeatherData | undefined} props.weatherData The weather data for the selected day, including warnings.
+ * @param {() => void} props.onClose Callback function triggered when the modal is closed.
+ *
+ * @returns {JSX.Element} The rendered modal with weather and warning details.
+ */
+const WeatherDayModal = ({ location, weatherData, onClose }: WeatherDayModalProps): JSX.Element => {
   return (
-    <Modal show={show} onHide={onClose} dialogClassName="weather-day-modal">
+    <Modal show={!!weatherData} onHide={onClose} dialogClassName="weather-day-modal">
       <Modal.Body className="d-flex justify-content-center text-center">
         <Container>
           <Row>
@@ -51,13 +82,13 @@ const WeatherDayModal = ({ show, onClose, location, weatherData }: WeatherDayMod
               </Button>
             </Col>
             <Col xs={8} className="d-flex align-items-center justify-content-center location-title">
-              <h3>{location}</h3>
+              <h6>{location}</h6>
             </Col>
           </Row>
-          <Row className={`mt-${weatherData.warnings.length ? 0 : 3}`}>
-            <WeatherCard type="EXPANDED" data={weatherData} />
+          <Row className={`mt-${weatherData?.warnings.length ? 0 : 3}`}>
+            {weatherData && <WeatherCard type="EXPANDED" data={weatherData} />}
           </Row>
-          {weatherData.warnings.length ? (
+          {weatherData?.warnings.length ? (
             <Row className="warnings-container mt-3 pt-2 d-flex justify-content-center align-items-center text-center">
               <Row className="text-center mb-2">
                 <Col>
@@ -73,7 +104,7 @@ const WeatherDayModal = ({ show, onClose, location, weatherData }: WeatherDayMod
             </Row>
           ) : (
             <Row className="warnings-container mt-4 pt-2 d-flex justify-content-center align-items-center text-center">
-              <Row className="text-center mt-2">
+              <Row className="text-center mt-4">
                 <Col className="warnings-title-empty">Sem Avisos Meterológicos</Col>
               </Row>
             </Row>
