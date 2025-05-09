86f417d98102c6605746aad834e165e1b4967d4e
luis-gouveia <luis.gouveia2801@gmail.com>
2025-05-09 10:00:57 +0100

    chore: useForecastAPI and other components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,55 @@
 import { useState } from 'react'
-import icon from '/icon128.png'
-import './App.css'
-import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
+import { WeatherData } from './types/WeatherData'
+import { useForecastAPI } from './hooks/useForecastAPI'
+import { useLocalStorage } from './hooks/useLocalStorage'
 import ThemeSwitch from './components/ThemeSwitch/ThemeSwitch'
+import LocationSelector from './components/LocationSelector/LocationSelector'
+import WeatherCard from './components/WeatherCard/WeatherCard'
+import WeatherDayModal from './components/WeatherDayModal/WeatherDayModal'
+import LoadingCard from './components/LoadingCard/LoadingCard'
+import ErrorCard from './components/ErrorCard/ErrorCard'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useLocalStorage('location', 'Lisboa')
+  const [isLoading, data, error] = useForecastAPI(location)
+
+  const [modalData, setModalData] = useState<WeatherData | undefined>(undefined)
+  const showModal = (weatherData: WeatherData) => setModalData(weatherData)
+  const closeModal = () => setModalData(undefined)
 
   return (
-    <Container className="app text-center pt-5">
-      <Row className="bg-success">
-        <Col xs={{ span: 8, offset: 2 }} className="d-flex justify-content-center align-items-center">
-          <h1>Funchal</h1>
-        </Col>
-        <Col xs="2" className="d-flex justify-content-center">
-          <ThemeSwitch />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-center">
-          <a href="https://vite.dev" target="_blank">
-            <img src={icon} className="logo" alt="Vite logo" />
-          </a>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1>Hello World</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button variant="primary" onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-        </Col>
-      </Row>
+    <Container className="app text-center pt-1">
+      {isLoading && <LoadingCard />}
+      {error && <ErrorCard error={error.message} />}
+      {data && (
+        <>
+          <Row>
+            <Col xs={{ span: 8, offset: 2 }} className="d-flex justify-content-center align-items-center">
+              <LocationSelector locations={data.locations} location={location} locationChangeHandler={setLocation} />
+            </Col>
+            <Col xs={2} className="d-flex justify-content-center">
+              <ThemeSwitch />
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col onClick={() => showModal(data.forecast[0])}>
+              <WeatherCard type="EXPANDED" data={data.forecast[0]} />
+            </Col>
+          </Row>
+          <Row className="mt-4 compact-cards-container">
+            {data.forecast.slice(1).map((forecast, index) => {
+              return (
+                <Col xs={3} className="compact-card" key={index} onClick={() => showModal(forecast)}>
+                  <WeatherCard type="COMPACT" data={forecast} />
+                </Col>
+              )
+            })}
+          </Row>
+          <WeatherDayModal onClose={closeModal} location={location} weatherData={modalData} />
+        </>
+      )}
     </Container>
   )
 }
